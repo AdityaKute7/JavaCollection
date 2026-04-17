8f22e6539f7d4b6d51ed64f15ef8bc3b248b28f9
AdityaKute7 <adikute0001@gmail.com>
2026-04-17 15:30:27 +0530

    LinkedList Basic

---
+++ LinkedListDemo.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+class LinkedListDemo{
+    public static void main(String[] args){
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);    
+        list.add(3);
+
+        
+
+        // System.out.println(list);
+        // list.addFirst(0);
+        // list.addLast(4);
+        // System.out.println(list);
+
+        // list.remove(3);
+        // System.out.println(list);
+
+        // list.removeIf(x-> x%2 ==0);
+        // System.out.println(list);
+
+        // LinkedList<String> list2 = new LinkedList<>(List.of("Hello", "World", "Java"));
+        // System.out.println(list2);
+
+        // LinkedList<String> list3 = new LinkedList<>(Arrays.asList("Hello", "World"));
+        
+        // list2.removeAll(list3);
+        // System.out.println(list2);
+    }
+}

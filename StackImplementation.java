32cc833a955f4b71a3ac5b2862737c2af12deba0
AdityaKute7 <adikute0001@gmail.com>
2026-04-17 18:29:56 +0530

    Edge Cases in StackImpl Methods

--- StackImplementation.java
+++ StackImplementation.java
@@ -1,39 +1,56 @@
 
-class Stack{
+class Stack {
     int[] stack = new int[5];
     int top = 0;
-    public void push(int data){
+
+    public void push(int data) {
+        if (top == 5) {
+            System.out.println("Stack is already full");
+        }
+        else{
         stack[top] = data;
-        top ++;
+        top++;
+        }
     }
-    public int pop(){
-        top --;
-        int data;
-        data = stack[top];
-        stack[top] = 0;
+
+    public int pop() {
+        int data = 0;
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+        } else {
+            top--;
+
+            data = stack[top];
+            stack[top] = 0;
+        }
         return data;
     }
 
-    public int size(){
+    public int size() {
         return top;
     }
 
-    public boolean isEmpty(){
-        return top<=0;
+    public boolean isEmpty() {
+        return top <= 0;
     }
-    public void show(){
-        for(int i : stack){
-            System.out.print(i +" ");
+
+    public void show() {
+        for (int i : stack) {
+            System.out.print(i + " ");
         }
     }
 }
+
 public class StackImplementation {
     public static void main(String[] args) {
         Stack stack = new Stack();
         System.out.println(stack.isEmpty());
+        stack.pop();
         stack.push(10);
         stack.push(20);
         stack.push(30);
+        stack.push(40);
+        stack.push(40);
         stack.push(40);
 
         stack.show();

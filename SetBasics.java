4dc2cfb1ad665d3c9a519704f570ee052d573b8a
AdityaKute7 <adikute0001@gmail.com>
2026-04-23 23:54:02 +0530

    Set Basic Oprations

---
+++ SetBasics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SetBasics {
+    
+
+
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(10); // duplicate ignored
+
+        System.out.println(set); // [10, 20]
+    }
+}
+
